859d2ba7cc3186e7ea5f9bf9075200f0f08337ec
fabio.morais <morais.fabiio@gmail.com>
2023-11-28 09:37:00 -0300

    implementação files comuns para todo o projeto

---
+++ app/src/main/java/com/example/list_currency_crypto/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.list_currency_crypto.common
+
+object Constants {
+    const val BASE_URL = "https://api.coinpaprika.com/"
+    const val PARAM_COIN_ID = "coinId"
+}
--- app/src/main/java/com/example/list_currency_crypto/presentation/MainActivity.kt
+++ app/src/main/java/com/example/list_currency_crypto/presentation/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.list_currency_crypto.presentation.ui.theme.ListcurrencycryptoTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,26 +18,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                ){
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ListcurrencycryptoTheme {
-        Greeting("Android")
     }
 }
